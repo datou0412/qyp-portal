bec69db41ae1800a9e4d4d55f074666a307a36b0
张 晓明 <zhangxiaoming@51xianqu.net>
2016-05-31 00:37:02 +0800

    实现了图片上传最简单的版本

--- app/controller/uploader-controller.js
+++ app/controller/uploader-controller.js
@@ -4,8 +4,8 @@ var _ = require('lodash');
 var formidable = require('formidable');
 var fs = require('fs');
 var uuid = require('node-uuid');
-var public_path='./'
-var host_name='http://www.dream623.com/u_img/'
+var public_path='./public/img/upload/'
+var host_name='/public/img/upload/'
 
 
 exports.upload = function* (next) {
@@ -14,8 +14,10 @@ exports.upload = function* (next) {
         var form = new formidable.IncomingForm();
         var img_path = uuid.v1()+".jpg";
         var fs_path=public_path+ img_path;
-        form.parse(request, function (error, fields, files) {
-            fs.renameSync(files.upload.path, fs_path);
+        form.parse(ctx.req, function (error, fields, files) {
+            console.log(files.imgFile.path);
+            //console.log(fields);
+            fs.renameSync(files.imgFile.path, fs_path);
         });
     ctx.locals.error = '0';
     ctx.locals.img_url = host_name+img_path;
